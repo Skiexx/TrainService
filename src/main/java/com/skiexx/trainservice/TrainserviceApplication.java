52e711a2115d4c1ea01928d58341b318a4031ae5
skiexx <codeskiexx@yandex.ru>
2023-07-02 17:08:38 +0700

    Init Commit

---
+++ src/main/java/com/skiexx/trainservice/TrainserviceApplication.java
@@ -0,0 +1,13 @@
+package com.skiexx.trainservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skiexx/trainservice/TrainserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skiexx.trainservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
